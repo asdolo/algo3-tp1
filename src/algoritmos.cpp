cfee5d5733d9409402057cdaade4db4b0ad91f77
asdolo <lraposeiras@dc.uba.ar>
2018-10-04 22:40:40 -0300

    WIP nuevo algoritmo de backtracking

--- src/algoritmos.cpp
+++ src/algoritmos.cpp
@@ -1,6 +1,8 @@
 #include <vector>
 #include <algorithm>
 #include "algoritmos.hpp"
+#include <iostream>
+#include <bitset>
 
 using namespace std;
 
@@ -60,70 +62,104 @@ int programacionDinamicaAux(vector<int> S, int i, int v)
 
 int backtracking(vector<int> S, int V)
 {
-	// Poda por factibilidad:
-	// 	Al analizar si el subconjunto actual suma el valor objetivo, podemos hacer un break
-	// cuando la sumatoria actual ya se pasa de dicho valor, ya que todos los valores son
-	// enteros no negativos
-
-	// Poda por optimalidad:
-	// Opción 1:
-	// 	Recorrer el conjunto de partes en orden creciente por cardinal.
-	// La primer solución que encontremos será una de las mínimas,
-	// con lo cual ya no conviene seguir buscando en el resto de subconjuntos
-	// (es imposible que haya otra solución con menor cardinal)
-	// Opción 2:
-	//	Recorrer el conjunto en cualquier orden. Cada vez que estamos por probar
-	// un subconjunto nuevo, vemos antes su cardinal. Si el cardinal es mayor que
-	// el mínimo que ya tenemos, lo salteamos, ya que, aunque el subconjunto sea
-	// solución, su cardinal será mayor que el ya obtenido.
-
-	int minimoCardinal = -1;
-
-	unsigned long long int cardinalPartes = getCardinalPartes(S.size());
-
-	// Para cada subconjunto de S... O(2^n)
-	for (unsigned long long int i = 0; i < cardinalPartes; i++)
+	
+	for (int i = 0; i <= S.size(); i++)	// O(n * ...)
 	{
-		vector<bool> binario = getBinario(i, S.size()); // O(n)
-		vector<int> subconjunto;
+		cout << endl << "Subconjuntos de tamaño " << i << " (" << nChoosek(S.size(), i) << "):" << endl;
+		// Subconjuntos de tamaño i
 
-		for (size_t j = 0; j < binario.size(); j++)
-		{
-			if (binario[j] == true)
-				subconjunto.push_back(S[j]);
-		}
-
-		// Poda por optimalidad: si el cardinal del subconjunto a analizar es mayor que el cardinal del
-		// subconjunto solución actual, entonces seguro que este subconjunto no será solución optima, con lo
-		// cual lo podemos saltear
-
-		if (minimoCardinal != -1 && (int)subconjunto.size() > minimoCardinal)
-			continue; // Avanzar al siguiente subconjunto
+		// Obtengo un vector de bool, que representa la primer permutacion de i elementos del vector original S
 		
-		// Hacemos la sumatoria del subconjunto
-		int sumaSubconjunto = 0;
+		// Hmm, qué pasa si esto es O(n)?
+		vector<bool> primeraPermutacion = getPrimeraPermutacion(i, S.size());
+		vector<bool> permutacionActual = getPrimeraPermutacion(i, S.size());
 
-		for (size_t j = 0; j < subconjunto.size(); j++)
-		{
-			sumaSubconjunto += subconjunto[j];
-			
-			// Poda por factibilidad: si nos pasamos del valor objetivo V,
-			// es imposible que, sumando más números no negativos, lleguemos al valor V
-			if (sumaSubconjunto > V)
-				break;
-		}
-			
-		if (sumaSubconjunto == V)
-		{
-			// Encontramos un subconjunto cuya sumatoria es el valor objetivo V
+		unsigned long long int currentPermCount = 1;
+		// Para cada subconjunto de tamaño i hacer:
+		do
+		{	
+			cout << currentPermCount << ": \t\t";
+			printVector(permutacionActual);
+			// Obtenemos el subconjunto en cuestión usando el vector de bool
+			// que representa la permutación de este subconjunto
+			vector<int> subconjunto;
 
-			// Veamos si es el minimo, o si es el primero que encontramos
-			if ((int)subconjunto.size() < minimoCardinal || minimoCardinal == -1)
-				minimoCardinal = subconjunto.size();
-		}
+			for (size_t j = 0; j < permutacionActual.size(); j++)
+			{
+				if (permutacionActual[j] == true)
+					subconjunto.push_back(S[j]);
+			}
+
+			// Hacemos la sumatoria del subconjunto
+			int sumaSubconjunto = 0;
+			bool solucionFactible = true;
+			int elementosNoFactibles = 0;
+
+			for (size_t j = 0; j < subconjunto.size() && solucionFactible; j++)
+			{
+				sumaSubconjunto += subconjunto[j];
+				
+				// Poda por factibilidad: si nos pasamos del valor objetivo V,
+				// es imposible que, sumando más números no negativos, lleguemos al valor V
+				if (sumaSubconjunto > V)
+				{
+					solucionFactible = false;
+					elementosNoFactibles = j;
+				}
+			}
+			
+			if (solucionFactible && sumaSubconjunto == V)
+			{
+				// Encontramos un subconjunto cuya sumatoria es el valor objetivo V
+
+				// Como estamos iterando en subconjuntos de tamaño i de forma creciente,
+				// seguro que, aunque haya una solución de tamaño mayor a i, ésta no va 
+				// a ser óptima, con lo cual nos ahorramos seguir buscando una solución
+				// óptima.
+
+				// Simplemente devolvemos ésta solución como (una) de las óptimas, pues lo es
+
+				cout << "Poda por optimalidad" << endl;
+				return subconjunto.size();
+			}
+			else
+			{
+				// Si la solución no es factible, guardamos en un vector las posiciones de los elementos
+				// que no son parte de la solucion
+				cout << "Poda por factibilidad" << endl;
+				// TODO: PREGUNTAR. Creo que está mal.
+			}			
+			
+			prev_permutation(permutacionActual.begin(), permutacionActual.end());
+			currentPermCount++;
+		} while (permutacionActual != primeraPermutacion);
+		
 	}
 
-	return minimoCardinal;
+	// Si llegamos acá es porque recorrimos todos los subconjuntos y no encontramos ninguna
+	// solución que sume el valor objetivo
+
+	return -1;
+}
+
+void printVector(vector<bool> v)
+{
+
+	if (v.size() == 0)
+	{
+		cout << "[ ]" << endl;
+		return;	
+	}
+
+	cout << "[";
+
+	for (int i = 0; i < v.size() - 1; i++)
+	{
+		cout << v[i] << ",";
+	}
+
+	cout << v[v.size() - 1] << "]" << endl;
+
 }
 
 int fuerzaBruta(vector<int> S, int V)
@@ -159,6 +195,20 @@ int fuerzaBruta(vector<int> S, int V)
 	return minimoCardinal;
 }
 
+unsigned nChoosek( unsigned n, unsigned k )
+{
+    if (k > n) return 0;
+    if (k * 2 > n) k = n-k;
+    if (k == 0) return 1;
+
+    int result = n;
+    for( int i = 2; i <= k; ++i ) {
+        result *= (n-i+1);
+        result /= i;
+    }
+    return result;
+}
+
 unsigned long long int getCardinalPartes(int length)
 {
 	unsigned long long int res = 1;
@@ -183,29 +233,17 @@ int getSumatoria(vector<int> items)
 	return res;
 }
 
-/*
-vector<vector<int>> getConjuntoDePartes(vector<int> items)
+vector<bool> getPrimeraPermutacion(int n, int length)
 {
-	long long int cardinalPartes = pow(2, items.size());
-	vector<vector<int>> res(cardinalPartes);
+	vector<bool> res(length);
 
-	for (int i = 0; i < cardinalPartes; i++)
+	for (int i = 0; i < n; i++)
 	{
-		vector<bool> binario = getBinario(i, items.size());
-		vector<int> subconjuntoActual;
-
-		for (size_t j = 0; j < binario.size(); j++)
-		{
-			if (binario[j] == true)
-				subconjuntoActual.push_back(items[j]);
-		}
-
-		res.push_back(subconjuntoActual);
+		res[i] = 1;
 	}
 
 	return res;
 }
-*/
 
 vector<bool> getBinario(int n, int length)
 {
--- src/algoritmos.hpp
+++ src/algoritmos.hpp
@@ -15,5 +15,8 @@ int fuerzaBruta(vector<int> S, int V);
 int backtracking(vector<int> S, int V);
 int programacionDinamica(vector<int> S, int V);
 int programacionDinamicaAux(vector<int> S, int i, int v);
+vector<bool> getPrimeraPermutacion(int n, int length);
+void printVector(vector<bool> v);
+unsigned nChoosek( unsigned n, unsigned k );
 
 #endif
